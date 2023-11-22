aaf8a6db6ffbd7d37c71c4216d07a0f5ae144c16
Ricardo <ricardoluiz_p@hotmail.com>
2023-11-22 20:33:22 -0300

    a

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     'facebookAuth': {
       'clientID': '373289728377262', // your App ID
       'clientSecret': '0a8854130cc42855ea0c59774d8a5de1', // your App Secret
-      'callbackURL': 'http://localhost:3000/auth/facebook/callback'
+      'callbackURL': 'https://fb-login-qqdg.onrender.com/auth/facebook/callback'
     }
   }
   
