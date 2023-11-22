2db429b3b86ec419e763218ea6b8d04040c6ff43
Ricardo <ricardoluiz_p@hotmail.com>
2023-11-22 20:25:46 -0300

    init

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'facebookAuth': {
+      'clientID': '373289728377262', // your App ID
+      'clientSecret': '0a8854130cc42855ea0c59774d8a5de1', // your App Secret
+      'callbackURL': 'http://localhost:3000/auth/facebook/callback'
+    }
+  }
+  
